fdf543d6a127d4d486a6c8054a4e44b271613c10
Jintao Sai <jts@navi-tech.net>
2022-05-11 13:29:18 +0800

    delete

--- src/config/common.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  commonUrl: 'https://test.navi-tech.net/api/tgm/',//api路径
-  urlVersion: '/1.0',//api版本
-  webSocket:'wss://test.navi-tech.net/tmws/data',
-  status:'wss://test.navi-tech.net/tmws/global'
-}
--- src/config/map.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    mapBoxAccessToken: 'pk.eyJ1Ijoic2FpamludGFvIiwiYSI6ImNrZXh2dHQyNjA1NGczMHBobnQ0bDM0ZGMifQ.dAO3yCa49vke7FVK7Saldw',//mapbox access token
-    zoom: 13,//地图初始缩放级别
-    center: [51.505, -0.09],//地图初始中心点
-    style: 'mapbox/light-v10',//地图样式
-    maxZoom: 18,//最大缩放级别
-    tileSize: 512,//瓦片数量 默认512×512像素
-    bigScreenStyle: 'mapbox/dark-v10',//地图样式
-}
